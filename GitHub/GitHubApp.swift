8166dff13291e351abff44436788d9b6b738abe2
David Melanson <david.melanson20@gmail.com>
2021-04-04 20:09:34 -0400

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by David Melanson on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
